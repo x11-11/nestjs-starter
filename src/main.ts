e4331b056f010981a17d02342321100b299bfeef
Zen Nguyen <nggiang12@gmail.com>
2023-03-04 15:40:03 -0800

    validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    //https://docs.nestjs.com/techniques/validation
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+    await app.listen(3000);
 }
 bootstrap();

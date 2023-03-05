0e34e73d5e90381032c92934a0c950e40400d95a
Zen Nguyen <nggiang12@gmail.com>
2023-03-04 17:13:55 -0800

    custom validator

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    //https://docs.nestjs.com/techniques/validation
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const app = await NestFactory.create(AppModule);
+  //https://docs.nestjs.com/techniques/validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-    await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();

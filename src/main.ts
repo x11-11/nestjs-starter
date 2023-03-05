f402374fe24231f85916fd267cde04ea3f0a6648
Zen Nguyen <nggiang12@gmail.com>
2023-03-04 18:32:33 -0800

    swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
-import { NestFactory } from '@nestjs/core';
+import fs from 'fs';
+import path from 'path';
+import { NestFactory, NestApplication } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  //https://docs.nestjs.com/techniques/validation
+  const app: NestApplication = await NestFactory.create(AppModule);
+
+  await setupSwagger(app);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
+/**
+ */
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+const packageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'),
+);
+
+async function setupSwagger(app: NestApplication) {
+  const { name, version, description } = packageJson;
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(name)
+    .setDescription(description)
+    .setVersion(version)
+    //.addTag('theapp')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+}
